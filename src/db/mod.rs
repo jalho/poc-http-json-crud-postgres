53008e9f53bd207f1ad08473844c0b1674b59911
jalho <alho.jere@gmail.com>
2025-09-18 23:41:56 +0300

    Refactor

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -8,52 +8,64 @@ use diesel::{Connection, SelectableHelper};
 pub struct Actor {
     term: crate::term::Handle,
 
-    connection: diesel::PgConnection,
-    chan_query: (tokio::sync::mpsc::Sender<Query>, tokio::sync::mpsc::Receiver<Query>),
+    db_connection: diesel::PgConnection,
+
+    tx_query: tokio::sync::mpsc::Sender<Query>,
+    rx_query: tokio::sync::mpsc::Receiver<Query>,
 }
 
 impl Actor {
     pub fn connect(term: crate::term::Handle, connection_string: &str) -> Result<Self, diesel::ConnectionError> {
-        let connection: diesel::PgConnection = diesel::pg::PgConnection::establish(connection_string)?;
+        let db_connection: diesel::PgConnection = diesel::pg::PgConnection::establish(connection_string)?;
+
+        let (tx_query, rx_query) = tokio::sync::mpsc::channel::<Query>(1);
 
         Ok(Self {
             term,
 
-            connection,
-            chan_query: tokio::sync::mpsc::channel::<Query>(1),
+            db_connection,
+
+            tx_query,
+            rx_query,
         })
     }
 
     pub fn get_handle(&self) -> tokio::sync::mpsc::Sender<Query> {
-        self.chan_query.0.clone()
+        self.tx_query.clone()
     }
 
     pub async fn work(mut self) -> Summary {
-        let job = async {
-            'recv_queries: loop {
-                let query_received: Query = match self.chan_query.1.recv().await {
-                    Some(n) => n,
-                    None => {
-                        break 'recv_queries;
-                    }
-                };
+        self.term
+            .token()
+            .run_until_cancelled(Self::handle_queries(&mut self.db_connection, &mut self.rx_query))
+            .await;
 
-                match query_received {
-                    Query::SelectManyBooks { respond_to } => {
-                        let db_query_result: Result<Vec<schema::Book>, diesel::result::Error> =
-                            books.select(schema::Book::as_select()).load(&mut self.connection);
+        Summary
+    }
 
-                        if let Err(_err) = respond_to.send(Response::new(db_query_result)) {
-                            eprintln!("failed to respond from DB client");
-                        }
+    async fn handle_queries(
+        connection: &mut diesel::PgConnection,
+        receiver: &mut tokio::sync::mpsc::Receiver<Query>,
+    ) -> () {
+        {
+            let query_received: Query = match receiver.recv().await {
+                Some(n) => n,
+                None => {
+                    return ();
+                }
+            };
+
+            match query_received {
+                Query::SelectManyBooks { respond_to } => {
+                    let db_query_result: Result<Vec<schema::Book>, diesel::result::Error> =
+                        books.select(schema::Book::as_select()).load(connection);
+
+                    if let Err(_err) = respond_to.send(Response::new(db_query_result)) {
+                        eprintln!("failed to respond from DB client");
                     }
                 }
             }
-        };
-
-        self.term.token().run_until_cancelled(job).await;
-
-        Summary
+        }
     }
 }
 
