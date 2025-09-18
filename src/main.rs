083078bb368a8e140b3c03416f8b93f30827a152
jalho <alho.jere@gmail.com>
2025-09-18 21:06:52 +0300

    Document

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 mod db;
 mod web;
 
+/// Proof-of-concept implementation demonstrating:
+///
+/// - HTTP JSON API with ergonomics of _serde_ ecosystem and _axum_
+/// - CRUD operations on PostgreSQL with ergonomics of _diesel_
+/// - Using _actor pattern_ in _tokio_ ecosystem
+///
+/// ### Cheatsheet
+///
+/// - Starting a containerized PostgreSQL instance:
+///
+///   ```console
+///   podman run --rm \
+///     --name poc-postgres \
+///     -e POSTGRES_PASSWORD=postgres \
+///     -p 127.0.0.1:5432:5432/tcp \
+///     docker.io/library/postgres:17.6-trixie@sha256:feff5b24fedd610975a1f5e743c51a4b360437f4dc3a11acf740dcd708f413f6
+///   ```
 fn main() -> std::process::ExitCode {
     let db: db::Actor =
         match db::Actor::connect("postgres://postgres:postgres@127.0.0.1:5432/postgres?connect_timeout=1") {
