6bba530b8624d19b26094880a919fab5c0a45f90
jalho <alho.jere@gmail.com>
2025-09-18 20:39:19 +0300

    Build runtime

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() -> std::process::ExitCode {
+    let runtime: tokio::runtime::Runtime = match tokio::runtime::Builder::new_current_thread().build() {
+        Ok(n) => n,
+        Err(err) => {
+            eprintln!("{err}");
+            return std::process::ExitCode::from(42);
+        }
+    };
+
     std::process::ExitCode::SUCCESS
 }
