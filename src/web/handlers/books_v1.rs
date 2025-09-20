1c83eecb118a2d8b642b1a2d9d7a84aab9cd7dc9
jalho <alho.jere@gmail.com>
2025-09-20 11:53:24 +0300

    Respond 204 instead of 200 when no response payload

--- src/web/handlers/books_v1.rs
+++ src/web/handlers/books_v1.rs
@@ -28,13 +28,13 @@ pub async fn get_one_by_id(
 pub async fn post_one(
     axum::extract::State(mut shared): axum::extract::State<crate::web::Shared>,
     axum::Json(book): axum::Json<crate::db::schema::Book>,
-) -> Result<(), axum::http::StatusCode> {
+) -> axum::http::StatusCode {
     let _rows_affected: usize = match shared.db_client.insert_book(book).await {
         Ok(n) => n,
         Err(_) => {
-            return Err(axum::http::StatusCode::INTERNAL_SERVER_ERROR);
+            return axum::http::StatusCode::INTERNAL_SERVER_ERROR;
         }
     };
 
-    Ok(())
+    return axum::http::StatusCode::NO_CONTENT;
 }
